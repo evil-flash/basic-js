185f66d416ca3a8367ebe1768717931ee3fd92ed
evil_FlasH <evil.flash@yandex.ru>
2023-04-16 21:16:41 +0300

    feat: complite 3 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+/* const { NotImplementedError } = require('../extensions/index.js'); */
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const value = parseFloat(sampleActivity);
+
+  if(typeof(sampleActivity) === 'string' && value < MODERN_ACTIVITY && value > 0){
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
+  }
+  return false
 }
 
 module.exports = {
